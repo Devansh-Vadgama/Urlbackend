67ce19e70cb17ac8ba6a95e4680512b7d9acf3c4
Devansh-Vadgama <devanshvadgama4@gmail.com>
2025-07-14 19:02:41 +0530

    d

--- index.js
+++ index.js
@@ -40,7 +40,7 @@ app.get('/:shortId', async (req, res) => {
   res.redirect(`${process.env.FRONTEND_URL}/ad/${req.params.shortId}`);
 });
 
-// GET: Get original URL (used in ad page)
+// GET: Get original URL
 app.get('/api/original/:shortId', async (req, res) => {
   const link = await Link.findOne({ shortId: req.params.shortId });
   if (!link) return res.status(404).json({ error: "Not found" });
